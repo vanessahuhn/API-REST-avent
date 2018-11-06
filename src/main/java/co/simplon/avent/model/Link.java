3241c45f9c77b47dd3b200186603d9159c696a0d
vanessahuhn <vanessa.huhn@gmail.com>
2018-11-06 11:14:37 +0100

    création enum et entité link + cardinalité

---
+++ src/main/java/co/simplon/avent/model/Link.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.simplon.avent.model;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author vanessa
+ */
+@Entity
+@Table(name = "link")
+public class Link {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String url;
+    
+    @Enumerated(EnumType.STRING)
+    private Topic topic;
+    
+    private LocalDate date;
+    
+    @ManyToOne
+    private User user;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
+    
+}
--- src/main/java/co/simplon/avent/model/User.java
+++ src/main/java/co/simplon/avent/model/User.java
@@ -5,10 +5,12 @@
  */
 package co.simplon.avent.model;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -26,6 +28,9 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    
+    @OneToMany
+    private List<Link> links;
 
     public String getFirstName() {
         return firstName;
